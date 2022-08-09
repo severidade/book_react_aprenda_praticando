236aa703cb4d5c9008e53ca3fcb8426667282bd4
severo <oliveira.severo@gmail.com>
2022-08-09 09:53:55 -0300

    feat: :rocket: Implementação da função que ordena livros  pelo titulo deforma crescente e decrescente

--- livro-react-maujor/tabela-livros/src/App.js
+++ livro-react-maujor/tabela-livros/src/App.js
@@ -21,15 +21,33 @@ class App extends Component {
 
   handleRemoverLinha = (id) =>{
     // console.log("Botão clicado")
-    const livros = this.state.livros.filter(l => l.id !== id);
     // cria um novo objeto excluíndo aquele cuja o id pertence ao item que deseja remover
+    const livros = this.state.livros.filter(l => l.id !== id);
+    this.setState({ livros });
+  };
+
+  handleOrdenarCrescente = (titulo) => {
+    const livros = this.state.livros.sort((a,b) =>
+      a.titulo < b.titulo ? -1:0
+    );
+    this.setState({ livros });
+  };
+
+  handleOrdenarDecrescente = (titulo) => {
+    const livros = this.state.livros.sort((a,b) =>
+      a.titulo < b.titulo ? -1:0
+    );
+    livros.reverse();
     this.setState({ livros });
   };
     
 	render() {
 		return (
 			<table className="tabela">
-				<TabelaHead />
+				<TabelaHead
+          ordenarCrescente={ this.handleOrdenarCrescente}
+          ordenarDecrescente={ this.handleOrdenarDecrescente}
+        />
 				<TabelaBody 
           livros={ this.state.livros }
           removerLinha={this.handleRemoverLinha}
--- livro-react-maujor/tabela-livros/src/componentes/TabelaHead.js
+++ livro-react-maujor/tabela-livros/src/componentes/TabelaHead.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-const TabelaHead = () => (
+const TabelaHead = (props) => (
   <thead>
     <tr>
-      <th colSpan="4">Tabela de Livros</th>
+      <th colSpan="4">
+        Tabela de Livros
+        <div class="container-setinhas">
+          <div onClick={ () => props.ordenarCrescente()} >&#129093;</div>
+          <div onClick={ () => props.ordenarDecrescente()} >&#129095;</div>
+        </div>
+      </th>
     </tr>
     <tr>
       <th>ISBN</th>
