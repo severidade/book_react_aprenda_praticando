06061a4adb57963c434c2cec81328d7b1046069e
severo <oliveira.severo@gmail.com>
2022-08-09 00:18:18 -0300

    feat: :rocket: Pegando os dados de um arquivo json externo como se fosse uma api

--- livro-react-maujor/tabela-livros/src/App.js
+++ livro-react-maujor/tabela-livros/src/App.js
@@ -5,25 +5,20 @@ import TabelaFoot from "./componentes/TabelaFoot";
 
 class App extends Component {
   state = {
-    livros: [
-      {
-        id: "978-85-7522-632-2",
-        titulo: "CSS Grid Layout",
-        autor: "Maurício Samy Silva"
-      },
-      {
-        id: "978-85-7522-677-3",
-        titulo: "Node Essencial",
-        autor: "Ricardo R. Lecheta"
-      },
-      {
-        id: "978-85-7522-512-7",
-        titulo: "Aprendendo Material Design",
-        autor: "Kyle Mew"
-      }
-    ]
+    livros: []
   };
-
+  componentDidMount() {
+    fetch("/api/livros.json")
+      .then(response => response.json())
+      .then(livros => this.setState({ livros }))
+      .catch(function(error) {
+        console.log("Erro na requisição");
+      })
+      .finally(function() {
+        console.log("Sempre retorna");
+      });
+    }
+    
 	render() {
 		return (
 			<table className="tabela">
